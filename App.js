e1e373dfe4de2f042b5b076397acfc9de4920478
Bayojid AI <bbostami385@gmail.com>
2026-03-02 10:37:00 +0600

    Initialize React app with routing and components
    Signed-off-by: Bayojid AI  <bbostami385@gmail.com>

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Feed from "./pages/index";
+import Chat from "./pages/chat";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Feed />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
