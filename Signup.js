fa12267a9a894df594c617382cb4502e9265e882
Bayojid AI <bbostami385@gmail.com>
2026-03-11 00:18:37 +0600

    Update Signup.js
    Signed-off-by: Bayojid AI  <bbostami385@gmail.com>

--- Signup.js
+++ Signup.js
@@ -1,24 +1,69 @@
 import React, { useState } from "react";
-import LoginLogo from "../assets/login_logo.png"; // same logo can be used
 
 function Signup() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
 
-  const handleSignup = () => {
-    console.log("Signup clicked", email, password);
-    // TODO: Firebase Auth integration
+  const [name,setName] = useState("");
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("");
+
+  const handleSignup = async () => {
+
+    const res = await fetch("/api/auth/signup",{
+
+      method:"POST",
+
+      headers:{
+        "Content-Type":"application/json"
+      },
+
+      body:JSON.stringify({
+        name,
+        email,
+        password
+      })
+
+    });
+
+    const data = await res.json();
+
+    alert(data.msg);
+
   };
 
-  return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <img src={LoginLogo} alt="Signup Logo" style={{ width: "150px" }} />
+  return(
+
+    <div style={{textAlign:"center"}}>
+
       <h2>Signup</h2>
-      <input type="email" placeholder="Email" onChange={e => setEmail(e.target.value)} />
-      <input type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
-      <button onClick={handleSignup}>Signup</button>
+
+      <input
+      placeholder="Name"
+      onChange={(e)=>setName(e.target.value)}
+      />
+
+      <br/><br/>
+
+      <input
+      placeholder="Email"
+      onChange={(e)=>setEmail(e.target.value)}
+      />
+
+      <br/><br/>
+
+      <input
+      type="password"
+      placeholder="Password"
+      onChange={(e)=>setPassword(e.target.value)}
+      />
+
+      <br/><br/>
+
+      <button onClick={handleSignup}>Create Account</button>
+
     </div>
+
   );
+
 }
 
 export default Signup;
