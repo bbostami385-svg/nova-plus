0af318c0cfbfb5dc9c6cf2c4fa2fd91211b2a38a
Bayojid AI <bbostami385@gmail.com>
2026-03-11 00:20:01 +0600

    Update Feed.js
    Signed-off-by: Bayojid AI  <bbostami385@gmail.com>

--- Feed.js
+++ Feed.js
@@ -1,12 +1,29 @@
 import React from "react";
 
-function Feed() {
-  return (
-    <div style={{ textAlign: "center" }}>
-      <h1>NovaPlus Social Feed 🚀</h1>
-      <p>All posts will appear here</p>
+function Feed(){
+
+  return(
+
+    <div style={{textAlign:"center"}}>
+
+      <h2>NovaPlus Feed</h2>
+
+      <div>
+
+        <p><b>User1:</b> Welcome to NovaPlus 🚀</p>
+
+      </div>
+
+      <div>
+
+        <p><b>User2:</b> This is our new social network</p>
+
+      </div>
+
     </div>
+
   );
+
 }
 
 export default Feed;
