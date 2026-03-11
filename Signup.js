5a8be0cd2e8ec68b8ffe290e122269c0719f365e
Bayojid AI <bbostami385@gmail.com>
2026-03-11 15:54:59 +0600

    Update Signup.js
    Signed-off-by: Bayojid AI  <bbostami385@gmail.com>

--- Signup.js
+++ Signup.js
@@ -1,69 +1,29 @@
 import React, { useState } from "react";
 
-function Signup() {
+function Signup(){
 
   const [name,setName] = useState("");
   const [email,setEmail] = useState("");
   const [password,setPassword] = useState("");
 
-  const handleSignup = async () => {
-
-    const res = await fetch("/api/auth/signup",{
-
-      method:"POST",
-
-      headers:{
-        "Content-Type":"application/json"
-      },
-
-      body:JSON.stringify({
-        name,
-        email,
-        password
-      })
-
-    });
-
-    const data = await res.json();
-
-    alert(data.msg);
-
-  };
-
   return(
-
     <div style={{textAlign:"center"}}>
 
-      <h2>Signup</h2>
+      <h2>Create Account</h2>
 
-      <input
-      placeholder="Name"
-      onChange={(e)=>setName(e.target.value)}
-      />
-
+      <input placeholder="Name" onChange={(e)=>setName(e.target.value)}/>
       <br/><br/>
 
-      <input
-      placeholder="Email"
-      onChange={(e)=>setEmail(e.target.value)}
-      />
-
+      <input placeholder="Email" onChange={(e)=>setEmail(e.target.value)}/>
       <br/><br/>
 
-      <input
-      type="password"
-      placeholder="Password"
-      onChange={(e)=>setPassword(e.target.value)}
-      />
-
+      <input type="password" placeholder="Password" onChange={(e)=>setPassword(e.target.value)}/>
       <br/><br/>
 
-      <button onClick={handleSignup}>Create Account</button>
+      <button>Signup</button>
 
     </div>
-
   );
-
 }
 
 export default Signup;
