a4a7a824f9827a85c6cfd2ef54a12bfa3d6e562c
Bayojid AI <bbostami385@gmail.com>
2026-03-12 12:26:57 +0600

    Update and rename Feed.js to src/Feed.js
    Signed-off-by: Bayojid AI  <bbostami385@gmail.com>

---
+++ src/Feed.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Feed() {
+  return (
+    <div style={{ textAlign: "center", padding: "20px" }}>
+      <h2>Feed</h2>
+      <p>Posts will appear here...</p>
+    </div>
+  );
+}
+
+export default Feed;
